90f39e3f9570ab6f4d48e508794a87e07f6d0a90
Lipsarani Mahanta <lipsaranimahanta21@gmail.com>
2026-09-11 23:30:48 +0530

    feat: add user entity

---
+++ User/src/main/java/com/e_wallet/User/entity/User.java
@@ -0,0 +1,25 @@
+package com.e_wallet.User.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String email;
+    private float balance;
+    private String Role;
+    private String password;
+}

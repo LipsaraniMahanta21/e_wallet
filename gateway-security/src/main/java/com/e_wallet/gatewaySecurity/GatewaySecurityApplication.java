029da275b646b3499a1a5505a68ab6a683f4dfe4
Lipsarani Mahanta <lipsaranimahanta21@gmail.com>
2026-09-20 23:17:31 +0530

    fix: fix GatewaySecurityApplication package

--- gateway-security/src/main/java/com/e_wallet/gatewaySecurity/GatewaySecurityApplication.java
+++ gateway-security/src/main/java/com/e_wallet/gatewaySecurity/GatewaySecurityApplication.java
@@ -1,4 +1,4 @@
-package com.e_wallet.Transaction;
+package com.e_wallet.gatewaySecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

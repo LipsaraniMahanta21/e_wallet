8e57591df86123819fee632272b2f2c435fdc324
Lipsarani Mahanta <lipsaranimahanta21@gmail.com>
2026-09-23 00:30:04 +0530

    fix: add request mapping

--- Transaction/src/main/java/com/e_wallet/Transaction/controller/TransactionController.java
+++ Transaction/src/main/java/com/e_wallet/Transaction/controller/TransactionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/transaction")
 public class TransactionController {
     @Autowired
     TransactionService transactionService;
